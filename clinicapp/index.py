462e3c53641debb9ed6c10bcf15a50336a3edb2e
Tuan Hoang <tuanhoang5050@gmail.com>
2025-11-19 21:37:19 +0700

    content

--- clinicapp/index.py
+++ clinicapp/index.py
@@ -7,18 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    cate_id = request.args.get('cate_id')
 
 
     return render_template("index.html")
-# @app.route('/product/<int:id>')
-# def details(id):
-#     prod=dao.get_product_by_id(id)
-#     q = request.args.get('q')
-#     cate_id = request.args.get('cate_id')
-#     cates = dao.load_categories()
-#     prods = dao.load_products(q=q, cate_id=cate_id)
-#     return render_template("product-details.html",prod=prod,cates=cates,prods=prods)
+
 
 @app.route('/cate_id/<int:id>')
 def create_form(id):
